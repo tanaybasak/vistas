9b7a2882aec0d7403df07d2cbefb462ad407fe9f
Tanoy Basak <TBASAK@beckman.com>
2024-12-06 17:11:36 +0530

    changes main

--- src/pages/Branding/Branding.jsx
+++ src/pages/Branding/Branding.jsx
@@ -1,18 +1,21 @@
 import { Outlet } from "react-router-dom";
 import CardBrand from "../../components/Card";
-import Grid from '@mui/material/Grid2';
 import visitingImg from '../../assets/image.svg';
-import { Box } from "@mui/material";
+import letterhead from '../../assets/image1.svg';
+import notebook from '../../assets/image2.svg';
+import notepad from '../../assets/image3.svg';
+import envelop from '../../assets/image4.svg';
 import './Branding.css';
+
 const cardData = [
   { id: 1, title: "VISITING CARD", description: "This is Brand 1", image: visitingImg },
-  { id: 2, title: "LETTERHEAD", description: "This is Brand 2", image: visitingImg },
-  { id: 3, title: "NOTEBOOK", description: "This is Brand 3", image: visitingImg },
-  { id: 4, title: "NOTEPAD", description: "This is Brand 4", image: visitingImg },
+  { id: 2, title: "LETTERHEAD", description: "This is Brand 2", image: letterhead },
+  { id: 3, title: "NOTEBOOK", description: "This is Brand 3", image: notebook },
+  { id: 4, title: "NOTEPAD", description: "This is Brand 4", image: notepad },
   { id: 5, title: "PRESENTATION FOLDERS", description: "This is Brand 4", image: visitingImg },
-  { id: 5, title: "ID CARD", description: "This is Brand 4", image: visitingImg },
+  { id: 5, title: "ID CARD", description: "This is Brand 4", image: letterhead },
   { id: 5, title: "FLIP DESK CALENDAR", description: "This is Brand 4", image: visitingImg },
-  { id: 7, title: "ENVELOP", description: "This is Brand 4", image: visitingImg },
+  { id: 7, title: "ENVELOP", description: "This is Brand 4", image: envelop },
 
 ];
 
